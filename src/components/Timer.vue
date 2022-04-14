ddf4f41105346223a23c1c89a2b2a998f85518ee
Renan Paixão <renanlemos21@gmail.com>
2022-04-14 20:39:52 -0300

    feat: add state manipulation when timer modal is open

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,29 +1,34 @@
 <template>
 		<div class="container">
-			<h6 @click="isChoosing = true">{{timeToShow.minutes}}:{{timeToShow.seconds}}</h6>
-			<div class="overlay" v-show="isChoosing">
-				<div class="container__indicator"/>
-				<div class="c-inputs">
-					<input type="number" v-model="time.minutes" min="0" max="59">
-					<p>:</p>
-					<input type="number" v-model="time.seconds" min="0" max="59">
-					<div class="c-inputs__images">
-						<img src="@/assets/images/sucess-button.png" alt="choose values button" @click="isChoosing = false">
-						<img src="@/assets/images/close-button.png" alt="close button" @click="reset">
+			<h6 @click="openChooseTimeBox">{{timeToShow.minutes}}:{{timeToShow.seconds}}</h6>
+			<div class="overlay" v-if="isChoosing" @click.self="isChoosing = false">
+				<div class="modal-container">
+					<div class="container__indicator"/>
+						<div class="c-inputs">
+							<input type="number" v-model="time.minutes" min="0" max="59">
+							<p>:</p>
+							<input type="number" v-model="time.seconds" min="0" max="59">
+							<div class="c-inputs__images">
+								<img src="@/assets/images/sucess-button.png" alt="choose values button" @click="isChoosing = false">
+								<img src="@/assets/images/close-button.png" alt="close button" @click="reset">
+							</div>
 					</div>
 				</div>
 			</div>
-		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
 import { computed, reactive, ref, watch } from 'vue'
+import { useStore } from 'vuex'
 
-const props = defineProps<{stop: boolean}>()
+const store = useStore()
+const { screenState }  = store.state
 const time:any = reactive({
 	minutes: 0,
 	seconds: 5
 })
+
 const timeToShow = computed(()=>{
 	const minutes = time.minutes < 10 ? '0' + time.minutes : time.minutes
 	const seconds = time.seconds < 10 ? '0' + time.seconds : time.seconds
@@ -31,10 +36,23 @@ const timeToShow = computed(()=>{
 })
 
 const isChoosing = ref(false)
+
+function openChooseTimeBox(){
+	if(screenState.isInitialScreen){
+		isChoosing.value = true
+	}
+}
+
+watch(isChoosing, () =>{
+	store.commit('toggleIsTimerOpen')
+})
+
+// About current time and modal buttons actions
 const currentTime = {
 	minutes: time.minutes,
 	seconds: time.seconds
 }
+
 watch(isChoosing, ()=>{
 	currentTime.minutes = time.minutes
 	currentTime.seconds = time.seconds
@@ -47,7 +65,7 @@ function reset(){
 }
 let timeIn: NodeJS.Timer
 
-watch(()=> props.stop, (stop)=>{
+watch(()=> screenState.isStopped, (stop)=>{
 	if(stop){
 		clearInterval(timeIn)
 	}else {
@@ -57,7 +75,7 @@ watch(()=> props.stop, (stop)=>{
 	}
 })
 
-watch(()=>time.seconds, (_, prevTime)=>{
+watch(()=> time.seconds, (_, prevTime)=>{
 	if(prevTime === 0 && time.minutes !== 0){
 		time.minutes--
 		time.seconds = 59
@@ -70,19 +88,36 @@ watch(()=>time.seconds, (_, prevTime)=>{
 
 <style scoped lang="scss">
 h6{
-	font-size: 2rem;
+	font-size: 1.6rem;
 	width: 10rem;
+	margin-right: 1rem;
+	
 	text-align: center;
+	color: $color-text-yellow;
+	font-family: "Fira Code";
 }
 .overlay{
 	display: block;
-	background-color: transparent;
+	position: fixed;
+	
+	top: 0;
+	bottom: 0;
+	right: 0;
+	left: 0;
+	
+	background: rgba(0, 0, 0, 0.8);
 }
 .container{
 	display: flex;
 	justify-content: center;
 	flex-direction: column;
 	
+	.modal-container{
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+		top: 10rem;
+	}
 	.container__indicator{
 		width: 0;
 		height: 0;
@@ -90,7 +125,7 @@ h6{
 		border-right: 7px solid transparent;
 		
 		border-bottom: 7px solid $color-background;
-		margin-left: 4.5rem;
+		margin-left: 3.5rem;
 		transform: translateY(1px);
 	}
 }
@@ -102,7 +137,6 @@ h6{
 	align-items: center;
 	justify-content: center;
 	padding: 0.5rem 0.9rem;
-	margin-left: 1rem;
 	border-radius: 7px;
 	
 	color: $color-white;
