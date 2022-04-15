b195b6218dd2bdb567102e56cc8e2e033daa399b
Renan Paixão <renanlemos21@gmail.com>
2022-04-14 22:30:58 -0300

    fix(TextSpy): remove blinking on initial page

--- src/components/TextSpy.vue
+++ src/components/TextSpy.vue
@@ -7,11 +7,10 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, watch } from 'vue'
+import { watch } from 'vue'
 import { useStore } from 'vuex'
 
 const store = useStore()
-const { screenState } = store.state
 
 const id = ()=> Date.now()
 
@@ -22,12 +21,10 @@ const allLetters = document.getElementsByClassName('letters')
 let indicatorPosition = 0
 
 function removeIndicator(position: number){
-	
 	allLetters[position].classList.remove('indicator')
 }
 
 function addIndicator(position: number){
-	
 	allLetters[position].classList.add('indicator')
 }
 
@@ -35,20 +32,21 @@ function passIndicatorToNextLetter(){
 	removeIndicator(indicatorPosition)
 	indicatorPosition++
 }
-
-onMounted(()=>{
+watch(store.state.screenState, (state)=>{
 	//Start indicator blinking
-	setInterval(()=>{
-		addIndicator(indicatorPosition)
-		const timeout = setTimeout(()=>{
-			removeIndicator(indicatorPosition)
-			clearTimeout(timeout)
-		}, 400)
-	}, 1000)
+	if(!state.isStopped){
+		setInterval(()=>{
+			addIndicator(indicatorPosition)
+			const timeout = setTimeout(()=>{
+				removeIndicator(indicatorPosition)
+				clearTimeout(timeout)
+			}, 500)
+		}, 1000)
+	}
 })
 
-watch(() => screenState.isInitialScreen, (isInitialScreen)=>{
-	if(!isInitialScreen){
+watch(store.state.screenState, (state)=>{
+	if(!state.isInitialScreen){
 		document.addEventListener('keypress', ()=>{
 			passIndicatorToNextLetter()
 		})
